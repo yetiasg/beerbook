e70fda13ce9f8ea5e717d49b3e3dd4fdd73001f1
yetiasg <yetiasg@gmail.com>
2021-08-10 23:09:32 +0200

    Add sorting options, add close btn to details modal

--- frontend/app/src/pages/TheBeers/components/BeerDetails.vue
+++ frontend/app/src/pages/TheBeers/components/BeerDetails.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="details" @click="backToBeers">
     <div class="details-container" @click.stop>
+      <span class="closeModal" @click="backToBeers">x</span>
       <h3>{{selectedBeer}}</h3>
     </div>
   </div>
@@ -34,11 +35,30 @@ export default {
   }
 
   .details-container{
+    position: relative;
     background-color: white;
     width: 50rem;
     height: 35rem;
     border-radius: 10px;
     padding: 1rem;
+  }
+
+  .closeModal{
+    position: absolute;
+    right: 1rem;
+    font-size: 1.4rem;
+    cursor: pointer;
+    width: 2rem;
+    height: 2rem;
+    background-color: transparent;
+    text-align: center;
+    border-radius: 50px;
+    background-color: rgb(246, 227, 192);
+  }
+
+  .closeModal:hover{
+    background-color: rgb(241, 218, 172);
+    transition: ease-in 0.2s;
   }
 
 </style>
--- frontend/app/src/pages/TheBeers/components/SortingBar.vue
+++ frontend/app/src/pages/TheBeers/components/SortingBar.vue
@@ -1,7 +1,16 @@
  <template>
   <section class="sorting-bar">
+    <p>sortuj</p>
     <div class="sorting">
-      <h3>Sorting Bar</h3>
+      <ul>
+        <li>Cena</li>
+        <li>BLG</li>
+        <li>IBU</li>
+        <li>Data wypicia</li>
+        <li>A-Z</li>
+        <li>Z-A</li>
+        <li>Ocena</li>
+      </ul>
     </div>
     <div class="addBtn">
       <base-button mode="filledBtn">Dodaj piwo</base-button>
@@ -21,7 +30,14 @@ export default {
     height: 6rem;
     display: flex;
     flex-direction: row;
-    margin: 1rem;
+    margin: 2rem 0 1rem 0;
+    position: relative;
+  }
+
+  .sorting-bar p{
+    position: absolute;
+    top: -1.7rem;
+    left: 0.3rem;
   }
 
   .sorting{
@@ -32,5 +48,20 @@ export default {
     background-color: rgb(255, 255, 255);
     border-radius: 10px;
     box-shadow: 0 0 5px rgba(49, 49, 49, 0.15);
+  }
+
+  ul{
+    height: 100%;
+    width: 100%;
+    list-style-type: none;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+
+  li{
+    padding:0.3rem;
+    cursor: pointer;
   }
 </style>
